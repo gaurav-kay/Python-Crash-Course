364969c61bb5c22feb53615876c59865d9392dff
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-09-08 09:20:57 +0530

    Added python file

---
+++ Notebook.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Crash Course!    
+#      
+#      
+
+# Using the terminal from **Jupyter** notebook
+
+# In[ ]:
+
+
+
+
+
+# Running python scripts from files
+
+# In[ ]:
+
+
+
+
+
+# *Hello World!*
+
+# In[ ]:
+
+
+
+
+
+# ### Variables in Python
+
+# In[ ]:
+
+
+
+
+
+# Type casting
+
+# In[ ]:
+
+
+
+
+
+# Basic Types
+#  - int
+#  - float
+#  - str
+#  - bool
+#  - list
+#  - tuples
+#  - set
+#  - dict
+
+# In[ ]:
+
+
+
+
+
+# Unpacking
+
+# In[ ]:
+
+
+
+
+
+# ### Lists
+# 
+# Methods
+#  - append()
+#  - insert()
+#  - pop()
+#  - remove()
+#  - clear()
+#  - sort()
+
+# In[ ]:
+
+
+
+
+
+# ### List slicing
+
+# In[ ]:
+
+
+
+
+
+# ### Loops in Python
+
+# In[ ]:
+
+
+
+
+
+# Membership operator
+
+# In[ ]:
+
+
+
+
+
+# Comments in Python
+
+# In[ ]:
+
+
+
+
+
+# pass statements
+
+# In[ ]:
+
+
+
+
+
+# ### if statements
+
+# In[ ]:
+
+
+
+
+
+# ### Functions in Python
+
+# In[ ]:
+
+
+
+
+
+# ### Indentation in Python
+# 
+# ```python
+# def function(l):
+#         # Example function
+#     if len(l) <= 1:
+#                   return 2 * [l]
+#     r = []
+#     for i in range(len(l)):
+#              s = l[:i] + l[i+1:]
+#              p = another_function(s)
+#              for x in p:
+#               r.append(l[i:i+1] + x)
+#     return r
+# ```
+
+# In[ ]:
+
+
+
+
+
+# ### Dictionaries and Sets
+
+# In[ ]:
+
+
+
+
+
+# 
